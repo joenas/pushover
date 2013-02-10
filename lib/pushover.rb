ac7c926a01138ffa5c42202135ccb639d6d2a6d8
Jon Neverland <jonwestin@gmail.com>
2013-02-10 23:07:58 +0100

    moved stuff around

--- lib/pushover.rb
+++ lib/pushover.rb
@@ -7,41 +7,53 @@ module Pushover
   # Makes it nice and easy to send Pushover notifications
   class Pushover < Thor
     include Thor::Actions
+
     CONF_DIR = File.join(Dir.home, '.pushover')
     CONF_FILE = "#{CONF_DIR}/config.yml"
     CONF_DEFAULTS = { 'user_key' => '', 'api_token' => ''}
     HOST_NAME = %x(hostname).chomp
+    DEFAULT_TITLE = "Message from #{HOST_NAME}"
 
     method_option :priority, :aliases => "-p", :default => 0, :desc => '1: Urgent, 0: Ordinary, -1: No sound/vibe'
-    method_option :title, :aliases => "-t", :default => "Message from #{HOST_NAME}"
+    method_option :title, :aliases => "-t", :desc => "your message's title, otherwise your app's name is used"
     method_option :devise, :aliases => "-d"
-    method_option :url
-    method_option :url_title
-    desc 'notify MSG', 'Send a Pushover notification!'
+    method_option :url, :desc => 'a supplementary URL to show with your message'
+    method_option :url_title, :desc => 'a title for your supplementary URL, otherwise just the URL is shown'
+
+    desc 'notify MSG', "Send a Pushover notification! Try 'pushover help notify' for available options."
     def notify(message)
-      initialize_config
-      client = Rushover::Client.new(@config['api_token'])
-      resp = client.notify(@config['user_key'], message, options)
-      if resp.ok?
-        say_status :ok, 'Notification sent!', :green
-      else
-        handle_error resp
-      end
+      @config = initialize_config
+      send_notification message, options
     rescue SocketError
       say_status :error, "Are you connected to the internetz?", :red
       exit 1
     end
 
   private
-    def initialize_config
-      if (File.exists? CONF_FILE)
-        @config = Psych.load_file(CONF_FILE)
+    def send_notification message, options
+      parse_options options
+
+      client = Rushover::Client.new(@config['api_token'])
+      @resp = client.notify(@config['user_key'], message, @params)
+      if @resp.ok?
+        say_status :ok, 'Notification sent!', :green
       else
-        create_config
+        handle_error
       end
     end
 
-    def create_config
+    def parse_options frozen_hash
+      @params = {}
+      @params[:title] = frozen_hash[:title] || @config['default_title'] || DEFAULT_TITLE
+      @params.merge frozen_hash
+    end
+
+    def initialize_config
+      return Psych.load_file(CONF_FILE) if (File.exists? CONF_FILE)
+      create_config_and_exit
+    end
+
+    def create_config_and_exit
       if yes? "Config does not exist, do you want to create it?"
         create_file CONF_FILE, Psych.dump(CONF_DEFAULTS)
         say "Please fill in Pushover API key and your name!"
@@ -51,11 +63,11 @@ module Pushover
       exit 1
     end
 
-    def handle_error(resp)
-      if resp[:user] == 'invalid' || resp[:token] == 'invalid'
+    def handle_error
+      if @resp[:user] == 'invalid' || @resp[:token] == 'invalid'
         append = ', check config.yaml'
       end
-      say_status :error, "#{resp[:errors].join()}#{append}", :red
+      say_status :error, "#{@resp[:errors].join()}#{append}", :red
     end
   end
 end
